1764941b3b793ab5a88b8f331112f69a8ee0e7a0
tejala2 <tejala2@illinois.edu>
2019-10-24 16:14:40 -0500

    Add new file

---
+++ infoScraper.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import numpy as np
+
+def get_random_ua():
+    random_ua = ''
+    ua_file = 'ua_file.txt'
+    try:
+        with open(ua_file) as f:
+            lines = f.readlines()
+        if len(lines) > 0:
+            prng = np.random.RandomState()
+            index = prng.permutation(len(lines) - 1)
+            idx = np.asarray(index, dtype=np.integer)[0]
+            random_proxy = lines[int(idx)]
+    except Exception as ex:
+        print('Exception in random_ua')
+        print(str(ex))
+    finally:
+        return random_ua
+
+user_agent = get_random_ua()
+headers = {
+    'user-agent': user_agent,
+    }
+url = 'https://www.apartments.com/off-campus-housing/il/champaign/university-of-illinois-at-urbana-champaign/?bb=t-lzw3l8rJ3346sK'
+response = requests.get(url, headers=headers)
+content = BeautifulSoup(response.content, "html.parser")
+
+nameAndAddressArr = []
+for location in content.findAll('header', attrs = {"class": "placardHeader"}):
+    nameAndAddressObject = {
+        "names": location.find('a', attrs={"class": "placardTitle js-placardTitle"}).get('title'),
+        "addresses": location.find('div', attrs={"class": "location"}).text.encode('utf-8').get('title'),
+        "propertyGroup": location.find('img', attrs={"class": "propertyLogo"}).get('alt'),
+    }
+    print(nameAndAddressObject)
+    nameAndAddressArr.append(nameAndAddressObject)
+print(nameAndAddressArr)
+with open('nameAndAddressData.json', 'w') as outfile:
+    json.dump(nameAndAddressArr, outfile)
