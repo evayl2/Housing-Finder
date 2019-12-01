2f2396f82524622827738991714e6c17c41af0b7
saketv2 <saketv2@illinois.edu>
2019-12-01 15:02:32 -0600

    Delete aptlisting.js

--- aptlisting.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const aptSchema = new mongoose.Schema({
-  name: String,
-  beds: Number,
-  baths: Number,
-  price: Number,
-  location: Number
-});
-module.exports = mongoose.model('Apartment Listing', aptSchema);
