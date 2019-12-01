ce83e84a69bfddf11f63e51047b30567ee94a81f
evayl2 <evayl2@illinois.edu>
2019-11-30 18:56:26 -0600

    Delete settings.py

--- Eva/templates/settings.py
+++
@@ -1,6 +0,0 @@
-from flask import request, redirect
-@app.route('/settings', methods = ['POST'])
-def signup():
-    email = request.form['email']
-    print("The email address is '" + email + "'")
-    return redirect('/')
