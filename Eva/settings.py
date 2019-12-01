192297e6d6a44b4cabd3c944625be206b7d13879
evayl2 <evayl2@illinois.edu>
2019-11-30 19:07:06 -0600

    updated some things

--- Eva/settings.py
+++ Eva/settings.py
@@ -3,5 +3,9 @@ from flask import request, redirect
 def signup():
     email = request.form['Email']
     name = request.form['Name']
+    major = request.form['Major']
+    price = request.form['Desired Price']
+    gender = request.form['Gender']
+    year = request.form['Year']
     print("The email address of '" + name + " is '" + email)
     return redirect('/')
